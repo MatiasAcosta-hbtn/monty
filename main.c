7f90c845d1282c7829d60c8e487a5fd0ed37c777
Matias Acosta <2277@holbertonschool.com>
2020-12-21 14:09:27 +0000

    Fix printf

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
     
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr,"USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	stream = fopen(argv[1], "r");
 	if (stream == NULL)
 	{
-		printf("Error: Can't open file %s\n",argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n",argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((nread = getline(&line, &len, stream)) != -1)
