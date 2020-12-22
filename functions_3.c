c79ef7bdbed40e9de9c4de4ffcb93497177e1289
Matias Acosta <2277@holbertonschool.com>
2020-12-22 15:30:19 +0000

    Finishing complete excelent

--- functions_3.c
+++ functions_3.c
@@ -43,21 +43,29 @@ void f_rotl(stack_s **stack, unsigned int line_number)
  **/
 void f_rotr(stack_s **stack, unsigned int line_number)
 {
-	stack_s *aux;
+	stack_s *aux = NULL, *new = NULL;
 	int stack_len = 0;
-	unsigned int temp, temp2;
 
 	(void)line_number;
 	stack_len = list_len(stack);
 	if (*stack != NULL && stack_len >= 2)
 	{
+		new = malloc(sizeof(stack_s));
+		if (!new)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			free_stack(*stack);
+			exit(EXIT_FAILURE);
+		}
+		new->prev = NULL;
 		aux = *stack;
-		temp = (*stack)->n;
-		while (aux->next)
+		while (aux->next->next)
 			aux = aux->next;
-
-		temp2 = aux->n;
-		aux->n = temp;
-		(*stack)->n = temp2;
+		new->n = aux->next->n;
+		free(aux->next);
+		aux->next = NULL;
+		aux = *stack;
+		new->next = aux;
+		*stack = new;
 	}
 }
